b686d4fbe34491874ee8043d2d1d22167dd828ee
RASOLOFONIAINA Mickael <mickael.rasolofoniaina@proximitybbdo.fr>
2019-04-08 21:45:37 +0300

    construct table header and table rows

--- src/mg/bici/ocr/core/Core.java
+++ src/mg/bici/ocr/core/Core.java
@@ -8,6 +8,12 @@ package mg.bici.ocr.core;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
+import mg.bici.ocr.exception.GenericException;
+import mg.bici.ocr.model.LocalisableNumber;
+import mg.bici.ocr.model.LocalisableWord;
+import mg.bici.ocr.model.TableHeader;
+import mg.bici.ocr.model.TableRow;
+import mg.bici.ocr.model.WordPosition;
 import net.sourceforge.tess4j.Tesseract;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -26,7 +32,16 @@ public class Core {
     private int pageSegMode = 1;
     private boolean preserveSpace = true;
     private int tableBreakerNumber = 2;
+    private PositionProvider positionProvider;
 
+    public PositionProvider getPositionProvider() {
+        return positionProvider;
+    }
+
+    public void setPositionProvider(PositionProvider positionProvider) {
+        this.positionProvider = positionProvider;
+    }
+    
     public int getTableBreakerNumber() {
         return tableBreakerNumber;
     }
@@ -111,10 +126,10 @@ public class Core {
         return Jsoup.parse(generateHtml(path));
     }
     
-    public Element getElement(Document document,String[] dictionnaries) {
+    public Element getElement(Element element,String[] dictionnaries) {
         Elements elements;
         for(int i = 0; i < dictionnaries.length; i++){
-            elements = document.select("span:contains(" + dictionnaries[i] + ")");
+            elements = element.select(String.format("span:contains(%s)", dictionnaries[i]));
             if(elements != null && elements.size() > 1) return elements.get(1);
         }
         return null;
@@ -127,30 +142,35 @@ public class Core {
     public Element getTableHeader(Document document) {
         return getDesignation(document).parent();
     }
-    
-    public List<Double> extractNumber(Element element) {
-        List<Double> result = new ArrayList();
+
+    // TODO: Doit supporter les entrées de type 20%,30$,100£,......
+    public List<LocalisableNumber> extractNumber(Element element) {
+        List<LocalisableNumber> result = new ArrayList();
         Elements childrens = element.children();
         if(!childrens.isEmpty()) {
             for(Element children:childrens) {  
                 try {
-                    result.add(Double.parseDouble(children.text()));
+                    result.add(new LocalisableNumber(
+                        children.text(),
+                        PositionProvider.getPosition(children)
+                    ));
                 }
-                catch(NumberFormatException ex) {}
+                catch(GenericException ex) {}
             }
         }
         return result;
     }
     
-    public int getNumberOfNumber(Element element) {
+    public int getNumberOfNumber(Element element) throws GenericException {
         return extractNumber(element).size();
     }
     
-    public boolean containsNumberMoreThan(Element element, int number) {
+    public boolean containsNumberMoreThan(Element element, int number) throws GenericException {
         return getNumberOfNumber(element) >= number;
     }
-      
-    public List<Element> getTableRows(Document document) {
+
+    // TODO: Trouver un moyen plus flexible de stopper la recherche des lignes
+    public List<Element> getTableRows(Document document) throws GenericException {
         Element tableHeader = getTableHeader(document);
         List<Element> result = new ArrayList<>();
         if(tableHeader != null) {
@@ -162,19 +182,61 @@ public class Core {
         return result;
     }
     
-    /*public double getTotalPrice(Element element) {
-        
-    }*/
+    public double getTotalPrice(List<LocalisableNumber> numbers) throws GenericException {
+        return Calculator.getTotalPrice(LocalisableNumber.getNumberOnly(numbers));
+    }
     
-    public static void main(String[] args) throws Exception {
+    public double getTotalPrice(Element element) throws GenericException {
+        return getTotalPrice(extractNumber(element));
+    }
+    
+    public TableHeader constructHeader(Document document) throws Exception {
+        Element tableHeader = getTableHeader(document);
+        PositionProvider pr = new PositionProvider(tableHeader);
+        
+        WordPosition quantityPosition = pr.getQuantityPosition();
+        WordPosition unitPricePosition = pr.getUnitPricePosition();
+        WordPosition totalPricePosition = pr.getTotalPricePosition();
+        WordPosition tvaPosition = pr.getTvaPosition();
+        WordPosition designationPosition = pr.getDesignationPosition();
+        
+        LocalisableWord quantity = new LocalisableWord(Dictionnary.QUANTITY_LABEL,quantityPosition);
+        LocalisableWord unitPrice = new LocalisableWord(Dictionnary.UNIT_PRICE_LABEL,unitPricePosition);
+        LocalisableWord totalPrice = new LocalisableWord(Dictionnary.TOTAL_PRICE_LABEL,totalPricePosition);
+        LocalisableWord tva = new LocalisableWord(Dictionnary.TVA_LABEL, tvaPosition);
+        LocalisableWord designation = new LocalisableWord(Dictionnary.DESIGNATION_LABEL, designationPosition);
+        
+        return new TableHeader(quantity,designation,unitPrice,totalPrice,tva);
+    }
+
+    // TODOS: Inclure toute les colonnes possibles
+    public List<TableRow> constructRows(TableHeader tableHeader, Document document) throws Exception {
+        List<Element> rows = getTableRows(document);
+        List<LocalisableNumber> numbers = null;
+        List<TableRow> result = new ArrayList();
+        TableRow tableRow = null;
+        for (Element row : rows) {
+            numbers = extractNumber(row);
+            tableRow = new TableRow();
+            tableRow.setQuantity(tableHeader.getQuantity().getNearest(numbers));
+            tableRow.setUnitPrice(tableHeader.getUnitPrice().getNearest(numbers));
+            tableRow.setTotalPrice(tableHeader.getTotalPrice().getNearest(numbers));
+            result.add(tableRow);
+        }
+        return result;
+    }
+
+    public List<TableRow> constructRows(Document document) throws Exception {
+        return constructRows(constructHeader(document), document);
+    }
+      
+    public static void main(String[] args) throws Exception,GenericException {
         Core core = new Core();
         Document document = core.generateDocument("modele-de-facture.pdf");
-        List<Element> rows = core.getTableRows(document);
-        for(Element row:rows) {
-            List<Double> numbers = core.extractNumber(row);
-            for(double number:numbers) {
-                System.out.println(number);               
-            }
+        List<TableRow> rows = core.constructRows(document);
+        for (TableRow row : rows) {
+            System.out.println(row);
         }
+
     }
 }
