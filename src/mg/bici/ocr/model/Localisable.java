ea4ef499c64d93008d820e7a5a1dfeee32b56580
RASOLOFONIAINA Mickael <mickael.rasolofoniaina@proximitybbdo.fr>
2019-04-08 21:44:32 +0300

    add localisable class

---
+++ src/mg/bici/ocr/model/Localisable.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mg.bici.ocr.model;
+
+
+/**
+ *
+ * @author Mickael
+ */
+public class Localisable {
+    private WordPosition wordPosition;
+
+    public WordPosition getWordPosition() {
+        return wordPosition;
+    }
+
+    public void setWordPosition(WordPosition wordPosition) {
+        this.wordPosition = wordPosition;
+    }
+}
---
+++ src/mg/bici/ocr/model/LocalisableWord.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mg.bici.ocr.model;
+
+import java.util.List;
+
+/**
+ *
+ * @author Mickael
+ */
+public class LocalisableWord extends Localisable {
+    private String word;
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+    
+    public LocalisableWord(String word,WordPosition wordPosition) {
+        this.setWord(word);
+        this.setWordPosition(wordPosition);
+    }
+
+    // TODO: Généraliser dans la classe mère
+    public LocalisableNumber getNearest(List<LocalisableNumber> localisables) {
+        LocalisableNumber result = null;
+        int gap = Integer.MAX_VALUE;
+        for (LocalisableNumber localisable : localisables) {
+            if (!(localisable.getWordPosition().getX2() < this.getWordPosition().getX1())) {
+                if (localisable.getWordPosition().getX2() - this.getWordPosition().getX2() <= gap) {
+                    gap = localisable.getWordPosition().getX2() - this.getWordPosition().getX2();
+                    result = localisable;
+                }
+            }
+        }
+        return result;
+    }
+}
